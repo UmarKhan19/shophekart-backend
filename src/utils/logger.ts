6e85f8142e0a1c4cf26c429ed5c8e8af14dd9810
Umar Khan <khanumar0108@gmail.com>
2024-10-09 17:12:29 +0530

    feat: Setup winston-mongodb to save logs in db

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,5 @@
 import { createLogger, format, transports } from "winston"
+import "winston-mongodb"
 import { ConsoleTransportInstance, FileTransportInstance } from "winston/lib/winston/transports"
 import util from "util"
 import config from "../config/config"
@@ -6,6 +7,7 @@ import { EApplicationEnvironment } from "../constants/application"
 import path from "path"
 import * as sourceMapSupport from "source-map-support"
 import { red, blue, yellow, green, magenta } from "colorette"
+import { MongoDBTransportInstance } from "winston-mongodb"
 
 // Linking trace support
 sourceMapSupport.install()
@@ -94,9 +96,21 @@ const fileTransport = (): Array<FileTransportInstance> => {
     ]
 }
 
+const mongodbTransport = (): Array<MongoDBTransportInstance> => {
+    return [
+        new transports.MongoDB({
+            level: "info",
+            db: config.DATABASE_URL as string,
+            metaKey: "meta",
+            expireAfterSeconds: 3600 * 24 * 30,
+            collection: "application-logs"
+        })
+    ]
+}
+
 export default createLogger({
     defaultMeta: {
         meta: {}
     },
-    transports: [...fileTransport(), ...consoleTransport()]
+    transports: [...fileTransport(), ...mongodbTransport(), ...consoleTransport()]
 })
