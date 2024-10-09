f08da51ae31e9f5d23277442e403ca0bd61e1b83
Umar Khan <khanumar0108@gmail.com>
2024-10-09 10:59:20 +0530

    feat: Helmet js installation

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import globalErrorHandler from "./middlewares/globalErrorHandler"
 import responseMessage from "./constants/responseMessage"
 import httpError from "./utils/httpError"
 import { healthRouter } from "./routers"
+import helmet from "helmet"
 
 const app: Application = express()
 
 //Middlewares
+app.use(helmet())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "../", "public")))
 
