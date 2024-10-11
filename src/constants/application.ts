94ed1b08938028dbe654c7e1a1257c609bc45295
Umar Khan <khanumar0108@gmail.com>
2024-10-11 15:00:15 +0530

    feat: Setup test environment

--- src/constants/application.ts
+++ src/constants/application.ts
@@ -2,7 +2,8 @@ import config from "../config/config"
 
 export enum EApplicationEnvironment {
     PRODUCTION = "production",
-    DEVELOPMENT = "development"
+    DEVELOPMENT = "development",
+    TEST = "test"
 }
 
 export const ALLOWED_ORIGINS: string[] = [config.CLIENT_URL as string]
