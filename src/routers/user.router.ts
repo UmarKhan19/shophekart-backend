4788baf6ef99d1cf08364e187cadf96fd53773b8
Umar <khanumar0108@gmail.com>
2024-10-13 17:14:35 +0530

    feat: Add route for getSingleUser controller

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,10 +1,46 @@
+/**
+ * User Routes
+ *
+ * This module exports a router that handles user-related routes.
+ *
+ * @module routers/user.router
+ */
+
 import { Router } from "express"
 import { validateSchema } from "../middlewares"
-import { registerUserSchema } from "../validation/user"
-import { registerUser } from "../controllers/user"
+import { getSingleUserSchema, registerUserSchema } from "../validation/user"
+import { registerUser, getSingleUser } from "../controllers/user"
 
-const router = Router()
+/**
+ * User router.
+ *
+ * @type {Router}
+ */
+const router: Router = Router()
 
+/**
+ * Registers a new user.
+ *
+ * This route validates the incoming request body against the `registerUserSchema`
+ * and then calls the `registerUser` controller to complete the registration process.
+ *
+ * @route POST /register
+ * @access public
+ */
 router.route("/register").post(validateSchema(registerUserSchema), registerUser)
 
+/**
+ * Retrieves a single user.
+ *
+ * This route validates the incoming query parameters against the `getSingleUserSchema`
+ * and then calls the `getSingleUser` controller to fetch the user data.
+ *
+ * @route GET /
+ * @access public
+ */
+router.route("/").get(validateSchema(getSingleUserSchema), getSingleUser)
+
+/**
+ * Exports the user router.
+ */
 export default router
