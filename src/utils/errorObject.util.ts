ea86f1cf1344e206cca4a6aa822e3dd4c99fe1c7
Umar <khanumar0108@gmail.com>
2024-10-13 15:00:20 +0530

    feat: Configure the errorObj to throw zod validation error

--- src/utils/errorObject.util.ts
+++ src/utils/errorObject.util.ts
@@ -4,6 +4,7 @@ import { config } from "../config"
 import { EApplicationEnvironment } from "../constants/application"
 import { logger } from "./"
 import { IHttpError } from "../types"
+import { ZodError } from "zod"
 
 // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 export default (error: Error | unknown, req: Request, errorStatusCode: number = 500): IHttpError<null> => {
@@ -12,14 +13,21 @@ export default (error: Error | unknown, req: Request, errorStatusCode: number = 
         statusCode: errorStatusCode,
         request: { method: req.method, url: req.originalUrl, ip: req.ip ?? null },
         data: null,
-        message: error instanceof Error ? error.message || responseMessage.SOMETHING_WENT_WRONG : responseMessage.SOMETHING_WENT_WRONG,
+        message:
+            error instanceof ZodError
+                ? error.errors[0].message
+                : error instanceof Error
+                  ? error.message || responseMessage.SOMETHING_WENT_WRONG
+                  : responseMessage.SOMETHING_WENT_WRONG,
         trace: error instanceof Error ? { error: error.stack } : null
     }
 
     // Log
-    logger.info(`CONTROLLER_ERROR`, {
-        meta: errorObj
-    })
+    if (config.ENV != EApplicationEnvironment.TEST) {
+        logger.info(`CONTROLLER_ERROR`, {
+            meta: errorObj
+        })
+    }
 
     // Production ENV check
     if (config.ENV === EApplicationEnvironment.PRODUCTION) {
