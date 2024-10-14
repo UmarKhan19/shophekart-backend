1bc0d272efc3d19deac04993326334708cf29532
Umar <khanumar0108@gmail.com>
2024-10-14 15:39:35 +0530

    feat: Add getNonce controller to get nonce

---
+++ src/controllers/user/getNonce.user.controller.ts
@@ -0,0 +1,51 @@
+/**
+ * Get Nonce User Controller
+ *
+ * This controller is responsible for generating and returning a nonce value.
+ * A nonce is a unique, one-time use number that can be used to prevent replay attacks.
+ *
+ * @module controllers/user/getNonce.user.controller
+ */
+
+import { Request, Response } from "express"
+import { asyncHandler, httpResponse } from "../../utils"
+import { generateNonceService } from "../../services/user"
+import { IMySessionData } from "../../types"
+import responseMessage from "../../constants/responseMessage"
+
+/**
+ * Get Nonce
+ *
+ * This function generates a nonce, stores it in the user's session data, and returns it in the response.
+ *
+ * @param {Request} req - The incoming HTTP request.
+ * @param {Response} res - The outgoing HTTP response.
+ */
+const getNonce = asyncHandler((req: Request, res: Response) => {
+    /**
+     * The user's session data.
+     * @type {IMySessionData}
+     */
+    const mySession: IMySessionData = req.session as IMySessionData
+
+    /**
+     * Generate a new nonce using the generateNonceService.
+     */
+    mySession.nonce = generateNonceService()
+
+    /**
+     * Set the Content-Type header to "text/plain" to indicate that the response body contains plain text.
+     */
+    res.setHeader("Content-Type", "text/plain")
+
+    /**
+     * Send a successful response with the generated nonce.
+     *
+     * @param {number} 200 - The HTTP status code for the response.
+     * @param {string} responseMessage.SUCCESSFUL_OPERATION - The message to be included in the response body.
+     * @param {string} mySession.nonce - The generated nonce to be included in the response body.
+     */
+    httpResponse(req, res, 200, responseMessage.SUCCESSFUL_OPERATION, { nonce: mySession.nonce })
+})
+
+export default getNonce
--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,2 +1,3 @@
 export { default as registerUser } from "./register.user.controller"
 export { default as getSingleUser } from "./getSingle.user.controller"
+export { default as getNonce } from "./getNonce.user.controller"
