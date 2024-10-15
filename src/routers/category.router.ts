7ddd62d8ce5b0965ded048fd3f0cfeb2173697b1
Piyush-Das <piyushdas3110@gmail.com>
2024-10-16 03:37:26 +0530

    feat: Add route file of category to app.ts

--- src/routers/category.router.ts
+++ src/routers/category.router.ts
@@ -1,15 +1,31 @@
 // category.route.ts
-import  { Router } from "express";
+
+import { Router } from "express";
 import { validateSchema } from "../middlewares";
+
+// Import the validation schema for creating a category
 import createCategorySchema from "../validation/category/createCategory.category.validation";
+
+// Import the controller for creating a category
 import createCategoryController from "../controllers/category/category.controller";
 
-const categoryRoute = Router();
+const categoryRouter: Router = Router();
 
-categoryRoute.post(
+/**
+ * Creates a new category.
+ *
+ * @route POST /create
+ * @access public
+ * @uses Middleware validateSchema to validate the request body against the `createCategorySchema`.
+ * @uses Controller createCategory to handle the creation process.
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @returns {Promise<void>}
+ */
+categoryRouter.post(
   "/create",
   validateSchema(createCategorySchema),
   createCategoryController
 );
 
-export default categoryRoute;
+export default categoryRouter;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,3 +1,4 @@
 export { default as healthRouter } from "./health.routers"
 export { default as userRouter } from "./user.router"
 export {default as productRouter} from "./product.router"
+export {default as categoryRouter} from "./category.router"
