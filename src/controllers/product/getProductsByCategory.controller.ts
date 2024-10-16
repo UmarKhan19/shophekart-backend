5b010914db4c8357bd5be34a3c73db61b1b11bb8
Piyush-Das <piyushdas3110@gmail.com>
2024-10-16 16:12:42 +0530

    feat: Add route to the productcateory

--- src/controllers/product/getProductsByCategory.controller.ts
+++ src/controllers/product/getProductsByCategory.controller.ts
@@ -7,7 +7,10 @@ const getProductsByCategoryController = asyncHandler(async (
   req: Request,
   res: Response,
 ) => {
-  const categoryName = req.params.categoryName as string;
+    const categoryName = req.params.categoryName.trim() as string;
+
+  // eslint-disable-next-line no-console
+  console.log(categoryName);
   const products = await getProductsByCategory(categoryName);
   httpResponse(req, res, 200, "Products fetched successfully", products);
 });
