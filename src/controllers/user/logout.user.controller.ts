dbd41aa86b0df0c9a7f8c3097decd27fece0afcc
Umar <khanumar0108@gmail.com>
2024-10-17 16:49:29 +0530

    feat: Add logout controller

---
+++ src/controllers/user/logout.user.controller.ts
@@ -0,0 +1,40 @@
+/**
+ * Handles the user logout functionality by destroying the session.
+ *
+ * @module controllers/user/logout.user.controller
+ */
+
+import { NextFunction, Request, Response } from "express"
+import { asyncHandler, httpError, httpResponse } from "../../utils"
+import responseMessage from "../../constants/responseMessage"
+
+/**
+ * Destroys the user's session to log them out.
+ *
+ * @param {Request} req - The Express Request object.
+ * @param {Response} res - The Express Response object.
+ * @param {NextFunction} next - The Express NextFunction object.
+ * @returns {void} Does not return any value.
+ *
+ * @throws {httpError} If an error occurs while destroying the session, an httpError is thrown.
+ */
+const logout = asyncHandler((req: Request, res: Response, next: NextFunction): void => {
+    /**
+     * Destroys the user's session.
+     *
+     * @param {function} callback - A callback function to handle the result of destroying the session.
+     */
+    req.session.destroy((error: Error) => {
+        /**
+         * If an error occurs while destroying the session, an httpError is thrown.
+         */
+        httpError(next, error, req, 500)
+    })
+
+    /**
+     * Sends a successful response to the client if the session is destroyed successfully.
+     */
+    httpResponse(req, res, 200, responseMessage.SUCCESSFUL_OPERATION, null)
+})
+
+export default logout
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
 import { validateSchema } from "../middlewares"
 import { getSingleUserSchema, registerUserSchema } from "../validation/user"
-import { registerUser, getSingleUser, getNonce, SiweAuthController, getProfile } from "../controllers/user"
+import { registerUser, getSingleUser, getNonce, SiweAuthController, getProfile, logout } from "../controllers/user"
 import verifyNonceParamsSchema from "../validation/user/verifyNonce.user.validation"
 
 const router: Router = Router()
@@ -15,5 +15,7 @@ router.route("/nonce").get(getNonce)
 router.route("/proile").get(getProfile)
 
 router.route("/verify").post(validateSchema(verifyNonceParamsSchema), SiweAuthController)
+
+router.route("/logout").post(logout)
 
 export default router
