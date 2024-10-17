d41728042f07b6637a2479cb7eabd0c2f85c711a
Piyush-Das <piyushdas3110@gmail.com>
2024-10-18 04:48:48 +0530

    feat: Update the fixed product service

--- src/services/product/fixedProduct/fixedProduct.product.service.ts
+++ src/services/product/fixedProduct/fixedProduct.product.service.ts
@@ -1,9 +1,20 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 // fixedProduct.service.ts
 import { FixedProduct } from "../../../models";
 import IFixedProductDocument from "../../../types/fixedProduct.type";
 
 const createFixedProduct = async (fixedProductData: Partial<IFixedProductDocument>): Promise<IFixedProductDocument> => {
   try {
+    // Check if a fixed product with the product ID already exists
+    const existingFixedProduct = await FixedProduct.findOne({
+      productId: fixedProductData.productId,
+    });
+
+    if (existingFixedProduct) {
+      throw new Error(`A fixed product with product ID ${fixedProductData.productId} already exists`);
+    }
+
+    // Create a new fixed product
     const fixedProduct = await FixedProduct.create({
       ...fixedProductData,
       productId: fixedProductData.productId,
