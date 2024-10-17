40e5dc0a08c58ed8f4edf6be56fb51ace5b5d9bc
Piyush-Das <piyushdas3110@gmail.com>
2024-10-18 04:21:28 +0530

    feat: Create a fixed Product service

---
+++ src/services/product/fixedProduct/fixedProduct.service.ts
@@ -0,0 +1,15 @@
+import { FixedProduct } from "../../../models";
+import IFixedProductDocument from "../../../types/fixedProduct.type";
+
+const getAllFixedProducts = async (): Promise<IFixedProductDocument[]> => {
+  try {
+    const fixedProducts = await FixedProduct.find()
+      .populate({ path: "productId", select: "_id name" })
+      .exec();
+    return fixedProducts;
+  } catch (error) {
+    throw new Error(`Failed to fetch fixed products: ${(error as Error).message}`);
+  }
+};
+
+export default getAllFixedProducts;
