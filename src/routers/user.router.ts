b3323122246f7caded0156b793bd746fc318aa25
Umar <khanumar0108@gmail.com>
2024-10-18 09:06:53 +0530

    feat: Add controller to delete logged in user

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
 import { validateSchema } from "../middlewares"
 import { getSingleUserSchema, registerUserSchema } from "../validation/user"
-import { registerUser, getSingleUser, getNonce, SiweAuthController, getProfile, logout } from "../controllers/user"
+import { registerUser, getSingleUser, getNonce, SiweAuthController, getProfile, logout, deleteLoggedInUser } from "../controllers/user"
 import verifyNonceParamsSchema from "../validation/user/verifyNonce.user.validation"
 
 const router: Router = Router()
@@ -12,10 +12,14 @@ router.route("/").get(validateSchema(getSingleUserSchema), getSingleUser)
 
 router.route("/nonce").get(getNonce)
 
-router.route("/proile").get(getProfile)
+router.route("/profile").get(getProfile)
 
 router.route("/verify").post(validateSchema(verifyNonceParamsSchema), SiweAuthController)
 
 router.route("/logout").post(logout)
+
+router.route("/me").delete(deleteLoggedInUser)
+
+// TODO: Route for admin to delete user
 
 export default router
