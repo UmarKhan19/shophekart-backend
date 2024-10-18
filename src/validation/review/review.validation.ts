4268dec7fbf6e3d45c12139e8310fcd540c11e90
Piyush-Das <piyushdas3110@gmail.com>
2024-10-18 06:44:43 +0530

    feat: Create review validation

---
+++ src/validation/review/review.validation.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const createReviewSchema = z.object({
+  targetId: z.string().uuid(),
+  targetType: z.enum(["product", "user"]),
+  reviewType: z.enum(["productReview", "userReview"]),
+  rating: z.number().min(1).max(5),
+  comment: z.string().min(1),
+});
+
+export default createReviewSchema;
