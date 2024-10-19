120a58f3d355614e0ba3ba0a34bbf4f7e13e058f
Umar <khanumar0108@gmail.com>
2024-10-19 11:34:42 +0530

    feat: Add controller to get single order

--- src/controllers/order/index.ts
+++ src/controllers/order/index.ts
@@ -1,3 +1,4 @@
 export { default as createOrder } from "./create.order.controller"
 export { default as getAllOrdersOfLoggedinUser } from "./getAllOfUser.order.controller"
 export { default as changeStatus } from "./changeStatus.order.controller"
+export { default as getSingleOrder } from "./getSingle.order.controller"
--- src/routers/order.router.ts
+++ src/routers/order.router.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { changeStatus, createOrder, getAllOrdersOfLoggedinUser } from "../controllers/order"
+import { changeStatus, createOrder, getAllOrdersOfLoggedinUser, getSingleOrder } from "../controllers/order"
 import { validateSchema } from "../middlewares"
-import { changeOrderStatusSchema, createOrderSchema } from "../validation/order"
+import { changeOrderStatusSchema, createOrderSchema, getSingleOrderSchema } from "../validation/order"
 
 const router = Router()
 
 router.route("/my").get(getAllOrdersOfLoggedinUser)
+router.route("/single/:id").get(validateSchema(getSingleOrderSchema), getSingleOrder)
 router.route("/create").post(validateSchema(createOrderSchema), createOrder)
 router.route("/update-status").post(validateSchema(changeOrderStatusSchema), changeStatus)
 
