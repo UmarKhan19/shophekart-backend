7fdcf222bf2aa8857222bff420aaa5f72f0ca74c
Umar <khanumar0108@gmail.com>
2024-10-19 10:50:28 +0530

    feat: Add update order status service

--- src/services/order/index.ts
+++ src/services/order/index.ts
@@ -1,2 +1,3 @@
 export { default as createOrderService } from "./create.order.service"
 export { default as getAllOrdersOfLoggedinUserService } from "./getAllOrdersOfLoggedinUser.order.service"
+export { default as updateOrderStatusService } from "./updateOrderStatus.order.service"
---
+++ src/services/order/updateOrderStatus.order.service.ts
@@ -0,0 +1,29 @@
+import { Document, Types } from "mongoose"
+import { ORDER_STATUS } from "../../constants/application"
+import { TOrder } from "../../types"
+import { Order } from "../../models"
+import responseMessage from "../../constants/responseMessage"
+import getSingleProduct from "../product/getSingleProduct.product.service"
+
+export default async function updateOrderStatusService(
+    orderId: Types.ObjectId,
+    sellerId: Types.ObjectId,
+    status: ORDER_STATUS
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+): Promise<(Document<unknown, {}, TOrder> & TOrder) | null> {
+    const order = await Order.findById(orderId)
+
+    if (!order) throw new Error(responseMessage.NOT_FOUND("Order"))
+
+    const product = await getSingleProduct(order.productId.toString())
+
+    if (!product) throw new Error(responseMessage.NOT_FOUND("Product"))
+
+    if ((product.sellerId as string).toString() !== sellerId.toString()) throw new Error(responseMessage.UNAUTHORIZED_ACCESS)
+
+    order.orderStatus = status
+
+    await order.save()
+
+    return order
+}
