b84538832c6e1bd3443b79d705e7626ba4713ee1
Umar <khanumar0108@gmail.com>
2024-10-19 10:53:41 +0530

    feat: Add validation for updating order status

---
+++ src/validation/order/changeStatus.order.validation.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+import { ORDER_STATUS_VALUES } from "../../constants/application"
+import validationErrorMessages from "../../constants/validationErrors"
+
+const changeOrderStatusSchema = z.object({
+    body: z.object({
+        status: z.enum(ORDER_STATUS_VALUES),
+        orderId: z
+            .string({
+                required_error: validationErrorMessages.MISSING_ENTITY("Order Id")
+            })
+            .uuid(validationErrorMessages.INVALID_ENTITY("Order Id"))
+    })
+})
+
+export default changeOrderStatusSchema
+
+export type TChangeOrderStatus = z.TypeOf<typeof changeOrderStatusSchema>
--- src/validation/order/index.ts
+++ src/validation/order/index.ts
@@ -1,1 +1,2 @@
 export { default as createOrderSchema } from "./create.order.validation"
+export { default as changeOrderStatusSchema } from "./changeStatus.order.validation"
