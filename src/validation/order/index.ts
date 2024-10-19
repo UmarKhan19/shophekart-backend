bbef05e7d0e721b7b3132e980c534f5c42e75277
Umar <khanumar0108@gmail.com>
2024-10-19 11:34:14 +0530

    feat: Add validation to get single order

--- src/validation/order/index.ts
+++ src/validation/order/index.ts
@@ -1,2 +1,3 @@
 export { default as createOrderSchema } from "./create.order.validation"
 export { default as changeOrderStatusSchema } from "./changeStatus.order.validation"
+export { default as getSingleOrderSchema } from "./getSingle.order.validation"
