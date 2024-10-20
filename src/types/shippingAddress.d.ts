70cde89599de23f8536a61ce5b20e276f7104c54
Umar <khanumar0108@gmail.com>
2024-10-20 23:39:40 +0530

    feat: Add mongoDB properties to type TShippingAddress

--- src/types/shippingAddress.d.ts
+++ src/types/shippingAddress.d.ts
@@ -1,43 +1,17 @@
+// src/types/shippingAddress.d.ts (1-49)
 import { Types } from "mongoose"
 
-/**
- * Represents a shipping address.
- */
 type TShippingAddress = {
-    /**
-     * The street address.
-     */
+    _id: Types.ObjectId
     address: string
-
-    /**
-     * The city or town.
-     */
     city: string
-
-    /**
-     * The state or province.
-     */
     state: string
-
-    /**
-     * The postal code or zip code.
-     */
     postalCode: string
-
-    /**
-     * The country of residence.
-     */
     country: string
-
-    /**
-     * The ID of the buyer associated with this shipping address.
-     */
     buyerId: Types.ObjectId
-
-    /**
-     * Whether this shipping address is the primary address for the buyer.
-     */
     isPrimary: boolean
+    createdAt: Date
+    updatedAt: Date
 }
 
 export default TShippingAddress
