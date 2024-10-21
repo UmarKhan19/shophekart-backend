6ed5473629503080649a9d0398537815f0f31c79
Piyush-Das <piyushdas3110@gmail.com>
2024-10-21 22:08:30 +0530

    feat: Add logic for stock reduction after order

--- src/controllers/order/create.order.controller.ts
+++ src/controllers/order/create.order.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextFunction, Request, Response } from "express"
@@ -17,7 +18,10 @@ const createOrder = asyncHandler(async (req: Request<{}, {}, TCreateOrder["body"
         httpError(next, new Error(responseMessage.NOT_FOUND("Product")), req, 404)
         return
     }
-
+    if (product.stock <= 0) {
+        httpError(next, new Error(responseMessage.NOT_FOUND("Stock")), req, 400);  // Assuming you have an OUT_OF_STOCK message
+        return;
+    }
     const order = await createOrderService({
         buyerId,
         deliveryBy,
@@ -27,7 +31,10 @@ const createOrder = asyncHandler(async (req: Request<{}, {}, TCreateOrder["body"
         tokenId,
         soldAtPrice: product.price ?? 0
     })
+    product.stock -= 1;
 
+    // Save the updated product
+    await product.save();
     httpResponse(req, res, 201, responseMessage.SUCCESSFUL_OPERATION("Order creation"), order)
 })
 
