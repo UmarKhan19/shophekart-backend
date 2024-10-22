6a25dae5ee749ced1ff87580d3d7bdef89ea9e79
Umar <khanumar0108@gmail.com>
2024-10-22 10:50:14 +0530

    fix: Service to fetch fixed price products to find products by id

--- src/services/product/fixedProduct/getSingleFixedProduct.service.ts
+++ src/services/product/fixedProduct/getSingleFixedProduct.service.ts
@@ -1,21 +1,19 @@
 // getSingleFixedProduct.service.ts
-import { FixedProduct } from "../../../models";
-import IFixedProductDocument from "../../../types/fixedProduct.type";
-import responseMessage from "../../../constants/responseMessage";
+import { FixedProduct } from "../../../models"
+import IFixedProductDocument from "../../../types/fixedProduct.type"
+import responseMessage from "../../../constants/responseMessage"
 
 const getSingleFixedProduct = async (fixedProductId: string): Promise<IFixedProductDocument> => {
-  try {
-    const fixedProduct = await FixedProduct.findOne({
-      productId: fixedProductId,
-    }).populate({ path: "productId"})
-      .exec();
-    if (!fixedProduct) {
-      throw new Error(responseMessage.NOT_FOUND("Fixed Product"));
-    }
-    return fixedProduct;
-  } catch (error) {
-    throw new Error(`Failed to get fixed product: ${(error as Error).message}`);
-  }
-};
+    try {
+        const fixedProduct: IFixedProductDocument = await FixedProduct.findById(fixedProductId).populate("category")
 
-export default getSingleFixedProduct;
+        if (!fixedProduct) {
+            throw new Error(responseMessage.NOT_FOUND("Fixed Product"))
+        }
+        return fixedProduct
+    } catch (error) {
+        throw new Error(`Failed to get fixed product: ${(error as Error).message}`)
+    }
+}
+
+export default getSingleFixedProduct
