45b3c109a88ec7161ac90a5e06174dcb8ec4656a
Umar <khanumar0108@gmail.com>
2024-10-24 22:35:20 +0530

    feat: Add mongoDB aggregation pipeline to reduce db calls to get product by category

--- src/services/product/getProductsByCategory.service.ts
+++ src/services/product/getProductsByCategory.service.ts
@@ -1,22 +1,27 @@
- 
- 
- 
-import { Product } from "../../models";
-import { IProductDocument } from "../../types";
-import { Category } from "../../models";
-import responseMessage from "../../constants/responseMessage";
+import { Product } from "../../models"
+import { IProductDocument } from "../../types"
 
 const getProductsByCategory = async (categoryName: string): Promise<IProductDocument[]> => {
-  try {
-    const category = await Category.findOne({ parentCategory: categoryName });
-    if (!category) {
-      throw new Error(responseMessage.NOT_FOUND("Category"));
+    try {
+        const products: IProductDocument[] = await Product.aggregate([
+            {
+                $lookup: {
+                    from: "categories",
+                    localField: "category",
+                    foreignField: "_id",
+                    as: "category"
+                }
+            },
+            {
+                $match: {
+                    "category.0.label": categoryName
+                }
+            }
+        ])
+        return products
+    } catch (error) {
+        throw new Error(`Failed to get products: ${(error as Error).message}`)
     }
-    const products = await Product.find({ category: category._id });
-    return products;
-  } catch (error) {
-    throw new Error(`Failed to get products: ${(error as Error).message}`);
-  }
-};
+}
 
-export default getProductsByCategory;
+export default getProductsByCategory
