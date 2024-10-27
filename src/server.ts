8501506bb91b93f53088370609dcbbe2b516bea5
Umar <khanumar0108@gmail.com>
2024-10-27 15:40:09 +0530

    feat: Add logger

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,47 @@
-/* eslint-disable no-console */
 import app from "./app"
 import { config } from "./config"
-// import { initRateLimiter } from "./config/rateLimiter.config"
-// import { EApplicationEnvironment } from "./constants/application"
+import { initRateLimiter } from "./config/rateLimiter.config"
+import { EApplicationEnvironment } from "./constants/application"
 import databaseService from "./services/database.service"
 import logger from "./utils/logger.util"
 
-const PORT = config.PORT || 8080
+const server = app.listen(config.PORT ?? 8080)
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+;(async () => {
+    try {
+        // Database connection
+        const connection = await databaseService.connect()
+        if (config.ENV != EApplicationEnvironment.TEST) {
+            logger.info(`DATABASE_CONNECTION`, {
+                meta: {
+                    CONNECTION_NAME: connection.name
+                }
+            })
+        }
 
-databaseService
-    .connect()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log("app is listening on port " + PORT)
+        initRateLimiter(connection)
+        if (config.ENV != EApplicationEnvironment.TEST) {
+            logger.info(`RATE_LIMITER_INITIATED`)
+            logger.info(`APPLICATION_STARTED`, {
+                meta: {
+                    PORT: config.PORT,
+                    SERVER_URL: config.SERVER_URL
+                }
+            })
+        }
+    } catch (error) {
+        if (config.ENV != EApplicationEnvironment.TEST) {
+            logger.error(`APPLICATION_CLOSED`, {
+                meta: { error }
+            })
+        }
+        server.close((error) => {
+            if (error && config.ENV != EApplicationEnvironment.TEST) {
+                logger.error(`APPLICATION_CLOSED`, {
+                    meta: { error }
+                })
+            }
+            process.exit(1)
         })
-    })
-    .catch((error) => {
-        logger.error("Error in connecting to database")
-        logger.error(error)
-    })
+    }
+})()
