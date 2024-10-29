f962aceae597e196a7267641aa6cd5acdca896f6
Umar <khanumar0108@gmail.com>
2024-10-29 17:42:53 +0530

    feat: Update domain url in app cookie config

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,7 @@ app.use(
         secret: "siwe-quickstart-secret",
         saveUninitialized: false,
         resave: false,
-        cookie: { maxAge: 60000 * 60 * 24, domain: config.CLIENT_URL },
+        cookie: { maxAge: 60000 * 60 * 24, domain: "shophekart-frontend-f81n.vercel.app" },
         store: sessionStore
     })
 )
