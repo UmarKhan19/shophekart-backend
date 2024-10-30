b170cd496fd9c1aadcd60834a9aeddc02b279e5a
Umar <khanumar0108@gmail.com>
2024-10-30 19:54:25 +0530

    feat: Add aggregation pipeline to get single order by order id

--- src/services/order/getSingle.order.service.ts
+++ src/services/order/getSingle.order.service.ts
@@ -1,11 +1,131 @@
 import { Types } from "mongoose"
 import { Order } from "../../models"
 import responseMessage from "../../constants/responseMessage"
+import { TOrderStatus } from "../../constants/application"
 
-export default async function getSingleOrderService(orderId: Types.ObjectId) {
-    const order = await Order.findById(orderId)
+export default async function getSingleOrderService(orderId: Types.ObjectId): Promise<TOrder> {
+    const order = await Order.aggregate<TOrder>([
+        {
+            $match: {
+                _id: orderId
+            }
+        },
+        {
+            $lookup: {
+                from: "shippingaddresses",
+                localField: "shippingAddress",
+                foreignField: "_id",
+                as: "shippingAddress"
+            }
+        },
+        {
+            $unwind: "$shippingAddress"
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "buyerId",
+                foreignField: "_id",
+                as: "buyer"
+            }
+        },
+        {
+            $unwind: "$buyer"
+        },
+        {
+            $lookup: {
+                from: "products",
+                localField: "productId",
+                foreignField: "_id",
+                as: "product"
+            }
+        },
+        {
+            $unwind: "$product"
+        },
+        {
+            $lookup: {
+                from: "categories",
+                localField: "product.category",
+                foreignField: "_id",
+                as: "product.category"
+            }
+        },
+        {
+            $unwind: "$product.category"
+        },
+        {
+            $project: {
+                // Fields directly from the order
+                orderStatus: 1,
+                soldAtPrice: 1,
+                shippingPrice: 1,
+                currencyType: "$product.currencyType",
+                deliveryBy: 1,
+
+                // Fields from the buyer
+                "buyer.walletAddress": 1,
+
+                // Fields from the product
+                "product.name": 1,
+                "product.images": 1,
+                "product.description": 1,
+                "product.category": "$product.category.label",
+                "product.type": 1,
+
+                // Fields from the shipping address (excluding buyerId and _id)
+                "shippingAddress.city": 1,
+                "shippingAddress.phoneNumber": 1,
+                "shippingAddress.lastName": 1,
+                "shippingAddress.country": 1,
+                "shippingAddress.postalCode": 1,
+                "shippingAddress.state": 1,
+                "shippingAddress.address": 1,
+                "shippingAddress.firstName": 1,
+                "shippingAddress.email": 1
+            }
+        }
+    ])
 
     if (!order) throw new Error(responseMessage.NOT_FOUND("Order"))
 
-    return order
+    return order[0]
+}
+
+type Address = {
+    state: string
+    address: string
+    lastName: string
+    email: string
+    country: string
+    postalCode: string
+    city: string
+    firstName: string
+    phoneNumber: string
+}
+
+type Buyer = {
+    walletAddress: `0x${string}`
+}
+
+type Product = {
+    description: string
+    images: string[]
+    type: "FixedProduct" | "Auction"
+    category: string
+    name: string
+}
+
+type OrderStatus = TOrderStatus
+
+type TOrder = {
+    _id: Types.ObjectId
+    deliveryBy: Date
+    soldAtPrice: number
+    product: Product
+    currencyType: string
+    orderStatus: OrderStatus
+    shippingAddress: Address
+    shippingPrice: number
+    buyer: Buyer
 }
