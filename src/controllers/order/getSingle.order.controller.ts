a864e93e11ca6f6b2c321b8bf2718dd4d4b5cdc8
Umar <khanumar0108@gmail.com>
2024-11-03 00:08:37 +0530

    feat: Add check to check if the caller is buyer or seller

--- src/controllers/order/getSingle.order.controller.ts
+++ src/controllers/order/getSingle.order.controller.ts
@@ -1,14 +1,22 @@
-import { Request, Response } from "express"
-import { asyncHandler, httpResponse } from "../../utils"
+import { NextFunction, Request, Response } from "express"
+import { asyncHandler, httpError, httpResponse } from "../../utils"
 import { Types } from "mongoose"
 import responseMessage from "../../constants/responseMessage"
 import { getSingleOrderService } from "../../services/order"
 import { TGetSingleOrder } from "../../validation/order/getSingle.order.validation"
+import { IMySessionData } from "../../types"
 
-const getSingleOrder = asyncHandler(async (req: Request, res: Response) => {
+const getSingleOrder = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const { orderId } = req.params as TGetSingleOrder["params"]
 
-    const order = await getSingleOrderService(new Types.ObjectId(orderId))
+    const mySession = req.session as IMySessionData
+
+    if (!mySession.userId) {
+        httpError(next, new Error(responseMessage.UNAUTHORIZED_ACCESS), req, 401)
+        return
+    }
+
+    const order = await getSingleOrderService(new Types.ObjectId(orderId), mySession.userId)
 
     httpResponse(req, res, 200, responseMessage.DATA_RETRIEVED_SUCCESSFULLY("Single order"), order)
 })
