0529a0aa64898e89d6afb3434b3dfc864c301497
Piyush-Das <piyushdas3110@gmail.com>
2024-11-04 17:24:03 +0530

    feat: Added update desc route in the user controllers

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,6 +3,7 @@ import { validateSchema } from "../middlewares"
 import { getSingleUserSchema, registerUserSchema } from "../validation/user"
 import { registerUser, getSingleUser, getNonce, SiweAuthController, getProfile, logout, deleteLoggedInUser } from "../controllers/user"
 import verifyNonceParamsSchema from "../validation/user/verifyNonce.user.validation"
+import updateDescription from "../controllers/user/updateDescription.user.controller"
 
 const router: Router = Router()
 
@@ -17,7 +18,7 @@ router.route("/profile").get(getProfile)
 router.route("/verify").post(validateSchema(verifyNonceParamsSchema), SiweAuthController)
 
 router.route("/logout").post(logout)
-
+router.route("/update-description").post(updateDescription)
 router.route("/me").delete(deleteLoggedInUser)
 
 // TODO: Route for admin to delete user
