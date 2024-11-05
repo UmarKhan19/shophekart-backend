5d972acd311d4888dbbdeb1451174d18c3e18844
Piyush-Das <piyushdas3110@gmail.com>
2024-11-05 21:11:11 +0530

    feat: Update user router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -4,6 +4,7 @@ import { getSingleUserSchema, registerUserSchema } from "../validation/user"
 import { registerUser, getSingleUser, getNonce, SiweAuthController, getProfile, logout, deleteLoggedInUser } from "../controllers/user"
 import verifyNonceParamsSchema from "../validation/user/verifyNonce.user.validation"
 import updateDescription from "../controllers/user/updateDescription.user.controller"
+import updateName from "../controllers/user/updateName.user.controller"
 
 const router: Router = Router()
 
@@ -19,6 +20,7 @@ router.route("/verify").post(validateSchema(verifyNonceParamsSchema), SiweAuthCo
 
 router.route("/logout").post(logout)
 router.route("/update-description").post(updateDescription)
+router.route("/update-name").post(updateName)
 router.route("/me").delete(deleteLoggedInUser)
 
 // TODO: Route for admin to delete user
